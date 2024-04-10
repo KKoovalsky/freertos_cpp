821871196ab174b5b848d07fe9f8c137efb807f9
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2024-04-10 14:57:45 +0200

    Fix valgrind warnings for FreeRTOS tests
    There are still some. They are due to executing `flag::set()` after the
    flag is destructed.

--- test/generic/test_thread_pool.cpp
+++ test/generic/test_thread_pool.cpp
@@ -86,35 +86,22 @@ TEST_CASE("Thread pool executes tasks", "[ThreadPool]") // NOLINT
 
     SECTION("Captures moved local objects")
     {
-        constexpr unsigned NumRuns{100};
-        constexpr unsigned ExpectedSuccessRatioPercent{90};
-        constexpr unsigned ExpectedSuccessfulRuns{NumRuns * ExpectedSuccessRatioPercent / 100};
-        constexpr unsigned MaxFailedRuns{NumRuns - ExpectedSuccessfulRuns};
+        auto flag{test::make_flag()};
+        std::string captured_string;
 
-        unsigned failed_runs{0};
-        for (unsigned i = 0; i < NumRuns; ++i)
         {
-            auto flag{test::make_flag()};
-            std::string captured_string;
-
-            {
-                std::string s{"MAKAPAKA SZASTA FASTA"};
-                pool.execute([&flag, &captured_string, str = std::move(s)]() {
-                    utils::delay(std::chrono::milliseconds{1});
-                    captured_string = str;
-                    flag.set();
-                });
-                REQUIRE(s.empty());
-            }
-
-            if (flag.is_set())
-                if (++failed_runs >= MaxFailedRuns)
-                    FAIL("Cannot capture local object in the background, num runs: " << i);
-
-            auto is_done{flag.wait_for(std::chrono::seconds{1})};
-            REQUIRE(is_done);
-            REQUIRE(captured_string == "MAKAPAKA SZASTA FASTA");
+            std::string s{"MAKAPAKA SZASTA FASTA"};
+            pool.execute([&flag, &captured_string, str = std::move(s)]() {
+                utils::delay(std::chrono::milliseconds{1});
+                captured_string = str;
+                flag.set();
+            });
+            REQUIRE(s.empty());
         }
+
+        auto is_done{flag.wait_for(std::chrono::seconds{1})};
+        REQUIRE(is_done);
+        REQUIRE(captured_string == "MAKAPAKA SZASTA FASTA");
     }
 
     SECTION("Executes multiple tasks in parallel")
